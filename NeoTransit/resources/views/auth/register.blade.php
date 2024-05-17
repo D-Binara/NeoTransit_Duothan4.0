c59aa1476a24606e8402a90a24e6aae134a63b49
Nipuni102 <116501264+Nipuni102@users.noreply.github.com>
2024-05-17 15:02:10 +0530

    log in and sign up

--- NeoTransit/resources/views/auth/register.blade.php
+++ NeoTransit/resources/views/auth/register.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container" >
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-8 ">
                 <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+                    <div class="card-header">{{ __('Sign Up') }}</div>
+                    <div class="content">
 
-                    <div class="card-body">
+                    <div class="card" >
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
-
+                            <div class="user-details">
+                            <div class="input-box">
                             <div class="row mb-3">
                                 <label for="firstname" class="col-md-4 col-form-label text-md-end">{{ __('First Name') }}</label>
 
@@ -24,7 +26,8 @@
                                     @enderror
                                 </div>
                             </div>
-
+</div>
+<div class="input-box">
                             <div class="row mb-3">
                                 <label for="lastname" class="col-md-4 col-form-label text-md-end">{{ __('Last Name') }}</label>
 
@@ -38,7 +41,8 @@
                                     @enderror
                                 </div>
                             </div>
-
+</div>
+<div class="input-box">
                             <div class="row mb-3">
                                 <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
@@ -52,7 +56,8 @@
                                     @enderror
                                 </div>
                             </div>
-
+                            </div>
+                            <div class="input-box">
                             <div class="row mb-3">
                                 <label for="mobileNumber" class="col-md-4 col-form-label text-md-end">{{ __('Mobile Number') }}</label>
 
@@ -66,21 +71,9 @@
                                     @enderror
                                 </div>
                             </div>
-
-                            <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
                             </div>
-
+                            
+                            <div class="input-box">
                             <div class="row mb-3">
                                 <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
@@ -94,7 +87,8 @@
                                     @enderror
                                 </div>
                             </div>
-
+                            </div>
+                            <div class="input-box">
                             <div class="row mb-3">
                                 <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
@@ -102,15 +96,42 @@
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                                 </div>
                             </div>
+                            </div>
+                            <div class="input-box">
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            
+                            </div>
+                            
+
+
+<div class="input-box">
 
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
+                                
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
+                                        {{ __('Sign Up') }}
+                                   
+                                    </div>
                                 </div>
+</div>
                             </div>
+
+</div>
                         </form>
+                    </div>
                     </div>
                 </div>
             </div>
